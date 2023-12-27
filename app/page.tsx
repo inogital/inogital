df002a1b90e0cd4640b7220c93d97101cee883f0
de-mawo <groovebassist1@gmail.com>
2023-12-27 11:23:48 +0200

    Fix Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 import HeroSection from "@/components/Home/HeroSection";
 import OurServices from "@/components/Home/OurServices";
 import CTA from "@/components/common/CTA";
-import Footer from "@/components/common/Footer";
-import Header from "@/components/common/Header";
 
 export default function Home() {
   return (
     <main className="">
-      <Header />
-      <HeroSection/>
-      <OurServices/>
-      <CTA/>
-      <Footer/>
+      <HeroSection />
+      <OurServices />
+      <CTA />
     </main>
   );
 }
