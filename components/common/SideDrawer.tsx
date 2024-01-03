cc0d81047cff5d0e8fba50bdef4ea70005c0e563
de-mawo <groovebassist1@gmail.com>
2024-01-03 22:08:40 +0200

    Fix header

--- components/common/SideDrawer.tsx
+++ components/common/SideDrawer.tsx
@@ -1,27 +1,20 @@
+"use client";
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
   DrawerHeader,
-  DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { TiThMenu } from "react-icons/ti";
 import Image from "next/image";
-import { Button } from "../ui/button";
 import Link from "next/link";
 import HeaderPopOver from "./HeaderPopOver";
 
 const SideDrawer = () => {
   return (
     <Drawer>
-      <DrawerTrigger>
-        <button className="p-2 bg-rose-100 rounded-full text-rose-500">
-          {" "}
-          <TiThMenu size={24} />{" "}
-        </button>
+      <DrawerTrigger className="p-2  bg-rose-100 rounded-full text-rose-500">
+        <TiThMenu size={24} />
       </DrawerTrigger>
       <DrawerContent className="fixed inset-0 h-screen mt-0 w-64 ">
         <DrawerHeader>
@@ -30,14 +23,14 @@ const SideDrawer = () => {
           </div>
         </DrawerHeader>
         <div className="flex  flex-col items-center mt-4 space-y-5 ">
-        <Link href="/" className="font-semibold text-lg text-slate-600">
-          Home
-        </Link>
-        <HeaderPopOver />
-        <Link href="/about" className="font-semibold text-lg text-slate-600">
-          About
-        </Link>
-      </div>
+          <Link href="/" className="font-semibold text-lg text-slate-600">
+            Home
+          </Link>
+          <HeaderPopOver />
+          <Link href="/about" className="font-semibold text-lg text-slate-600">
+            About
+          </Link>
+        </div>
       </DrawerContent>
     </Drawer>
   );
